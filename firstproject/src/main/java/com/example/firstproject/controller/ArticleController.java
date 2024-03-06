ce68a07789c729850b001715eee5f44d1148a252
HP657 <junlee5070@gmail.com>
2024-03-06 19:15:33 +0900

    H2 DB 사용

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,12 +1,17 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+    @Autowired
+    private ArticleRepository articleRepository;
     @GetMapping("/articles/new")
     public String newArticleForm() {
         return "articles/new";
@@ -15,6 +20,10 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
         System.out.println(form.toString());
+        Article article = form.toEntity();
+        System.out.println(article.toString());
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
         return "";
     }
 }
---
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,32 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+
+    public Article(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
